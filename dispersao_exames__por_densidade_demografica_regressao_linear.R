411f72ce276911e79487d6369139291c20924a0c
amandaventurac <41547014+amandaventurac@users.noreply.github.com>
2020-11-05 13:39:21 -0300

    Add files via upload

--- dispersao_exames__por_densidade_demografica_regressao_linear.R
+++ dispersao_exames__por_densidade_demografica_regressao_linear.R
@@ -1,21 +1,23 @@
-#setwd("diretorio_onde_estao_os_arquivos")
+setwd("/media/medea/Seagate Backup Plus Drive/outros_profissionais/portfolio_dsa/exames_papanicolau")
 media_estados <- read.table('media_exames_por_estado_com_densidade_demografica.txt', header = FALSE, sep = ",", dec = ".")
 media_estados
 library(ggplot2)
 library(ggrepel)
 p<-ggplot(data=media_estados, aes(x= V3, y= V2)) +
-  geom_point(stat="identity", color = 'purple', fill = "purple") +
+  geom_point(stat="identity", color = 'purple', fill = "purple", size = 3) +
   xlab(expression(paste("Densidade populacional (hab/km"^"2",")"))) +  ylab("Média de exames")
 p + ggtitle("Exames citopatológicos de colo do útero realizado pelo SUS \n em mulheres de 25 a 59 anos de 12/2008 a 12/2011 \n Média de exames e densidade populacional") + theme(plot.title = element_text(hjust = 0.5)) + 
-geom_text_repel(label= media_estados[,1], nudge_x = 0.2, nudge_y = 0.2) 
+geom_text_repel(label= media_estados[,1], nudge_x = 0.2, nudge_y = 0.2) +
+theme(text = element_text(size=17))
 
 #excluindo os outliers do modelo linear
 media_estados2 <- subset(media_estados, !(media_estados[,1]  %in% c("AM","MA","PA", "AP")))
 media_estados2
 
 p<-ggplot(data=media_estados, aes(x= V3, y= V2)) +
-  geom_point(stat="identity", color = 'purple', fill = "purple") +
+  geom_point(stat="identity", color = 'purple', fill = "purple", size = 3) +
   xlab(expression(paste("Densidade populacional (hab/km"^"2",")"))) +  ylab("Média de exames")
 p + ggtitle("Exames citopatológicos de colo do útero realizado pelo SUS \n em mulheres de 25 a 59 anos de 12/2008 a 12/2011 \n Média de exames e densidade populacional") + theme(plot.title = element_text(hjust = 0.5)) + 
 geom_smooth(data=media_estados2, aes(x= V3, y= V2), method  = 'lm', interval = "confidence", level = 0.95) +
-geom_text_repel(label= media_estados[,1], nudge_x = 0.3, nudge_y = 0.3)  
+geom_text_repel(label= media_estados[,1], nudge_x = 0.3, nudge_y = 0.3)  +
+theme(text = element_text(size=17)) 
